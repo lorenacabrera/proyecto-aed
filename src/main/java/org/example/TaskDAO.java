1e05230444d231d56f89383859934c378ca15549
lorena <marialorenacg@gmail.com>
2025-12-01 20:26:25 +0000

    CRUD con hibernate

--- src/main/java/org/example/TaskDAO.java
+++ src/main/java/org/example/TaskDAO.java
@@ -1,4 +1,39 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
 public class TaskDAO {
+
+    public void save(Task task) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Transaction tx = session.beginTransaction();
+            session.persist(task);
+            tx.commit();
+        }
+    }
+
+    public void update(Task task) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Transaction tx = session.beginTransaction();
+            session.merge(task);
+            tx.commit();
+        }
+    }
+
+    public void delete(Task task) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Transaction tx = session.beginTransaction();
+            session.remove(task);
+            tx.commit();
+        }
+    }
+
+    public List<Task> findAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("from Task", Task.class).list();
+        }
+    }
 }
